6067dfab67eedb3681dc4b13ff47c911199060e2
YnSkimmeeGaelgagh <107683249+YnSkimmeeGaelgagh@users.noreply.github.com>
2026-03-19 17:13:14 +0000

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,15 +1,21 @@
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+};
 document.body.style.overflow = "hidden";
 let gameStarted = false;
 const splashContainer = document.getElementById("splash-container");
 
 function startGame (event) {
-    splashContainer.style.visibility = "hidden";
-    gameStarted = true;
-    const topBarChildren = [...document.getElementById("top-bar").children];
-        topBarChildren[1].classList.add("title-anim");
-        topBarChildren[2].classList.add("hint-anim");
-        topBarChildren[3].classList.add("arrow-anim");
-    document.body.style.overflow = "auto";
+    event.target.classList.add("pressed-btn");
+    setTimeout(() => {
+        splashContainer.style.visibility = "hidden";
+        gameStarted = true;
+        const topBarChildren = [...document.getElementById("top-bar").children];
+            topBarChildren[1].classList.add("title-anim");
+            topBarChildren[2].classList.add("hint-anim");
+            topBarChildren[3].classList.add("arrow-anim");
+        document.body.style.overflow = "auto";  
+    }, 300);
 };
 
 const splashBtn = document.getElementById("splash-btn");
